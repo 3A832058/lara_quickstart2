144dab293b34f6a5ff484c49bfe531a304d3536a
Xin-Ting Ke <greenpepper0606@gmail.com>
2021-12-09 16:53:23 +0800

    Eloquent關聯

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,12 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    /**
+     * 取得該使用者的所有任務。
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
